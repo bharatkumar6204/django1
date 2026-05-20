f0c55424382be7f0dc63a4c6d4e76b91987845a0
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-05-21 01:13:18 +0530

    myproject

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app1.models import Student
 
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['st_name','st_age','st_email','st_number']
+admin.site.register(Student,StudentAdmin)
--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    st_name = models.CharField(max_length=100)
+    st_age = models.IntegerField()
+    st_email = models.EmailField(max_length=50)
+    st_number = models.IntegerField()
+
+    def __str__(self):
+        return self.st_name
+
+
+    
