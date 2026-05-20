a4ecd4bafdda2cef32af9a6cd2e3f39165c5cbca
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-05-21 00:04:59 +0530

    myproject

--- app1/views.py
+++ app1/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context ={
+        'name':'bharat','age':23, 'course': 'python full stack' # pass data to template
+    }
+    return render(request, 'home.html',context)
